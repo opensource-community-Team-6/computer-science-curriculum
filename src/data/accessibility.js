6d50cf887ba11d9d34cd6480fc8398f250cb1345
jaryapp <jaryapp@gmail.com>
2020-08-31 02:58:39 +0900

    [#1] feat: 충북대학교 소프트웨어학과 2학년

--- src/data/accessibility.js
+++ src/data/accessibility.js
@@ -60,6 +60,136 @@ export const accessibilityCollection = {
 				},
 			]
 		},
+		{
+			name:"2학년",
+			skills: [
+				{
+					name: "자료구조",
+					description: {
+						text: "컴퓨터과학 및 디지털정보처리 분야의 수학적 기초가 되는 논리, 함수, 관계를 포함한 집합, 알고리즘, 수학적 귀납법 및 증명방법, 그래프, 트리, 부울 대수 등을 학습한다.",
+						links: [
+	
+						]
+					}
+				},
+				{
+					name: "컴퓨터구조",
+					description: {
+						text: "",
+						links: [
 
+						]
+					}
+				},
+				{
+					name: "소프트웨어실전영어",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "객체지향 프로그래밍",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "선형대수학",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "미래설계구현",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "오픈소스 기초프로젝트",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "알고리즘",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "프로그래밍언어론",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "시스템프로그래밍",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "창업탐색",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "오픈소스개발프로젝트",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "확률및통계",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+				{
+					name: "컴퓨터그래픽스",
+					description: {
+						text: "",
+						links: [
+
+						]
+					}
+				},
+			]
+		}
 	]
 };
